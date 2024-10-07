e552a36a31c89a6affbd572ddc85625773d88513
shinesakir2 <m.zakir@adcda.gov.ae>
2024-10-07 09:44:12 +0400

    first push

---
+++ src/containers/Dashboard/DashboardLayout.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Container, Col, Row } from '@openedx/paragon';
+
+import WidgetSidebar from '../WidgetContainers/WidgetSidebar';
+
+import hooks from './hooks';
+
+export const columnConfig = {
+  courseList: {
+    withSidebar: {
+      lg: { span: 12, offset: 0 },
+      xl: { span: 8, offset: 0 },
+    },
+    noSidebar: {
+      lg: { span: 12, offset: 0 },
+      xl: { span: 12, offset: 0 },
+    },
+  },
+  sidebar: {
+    lg: { span: 12, offset: 0 },
+    xl: { span: 4, offset: 0 },
+  },
+};
+
+export const DashboardLayout = ({ children }) => {
+  const {
+    isCollapsed,
+    sidebarShowing,
+  } = hooks.useDashboardLayoutData();
+
+  const courseListColumnProps = sidebarShowing
+    ? columnConfig.courseList.withSidebar
+    : columnConfig.courseList.noSidebar;
+
+  return (
+    <Container fluid size="xl">
+      <Row>
+        <Col {...courseListColumnProps} className="course-list-column">
+          {children}
+        </Col>
+        <Col {...columnConfig.sidebar} className="sidebar-column">
+          {!isCollapsed && (<h2 className="course-list-title">&nbsp;</h2>)}
+          <WidgetSidebar />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+DashboardLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default DashboardLayout;
